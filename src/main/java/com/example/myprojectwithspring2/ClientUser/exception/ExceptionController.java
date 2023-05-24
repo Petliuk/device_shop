a05c79e7ebfa3c0d6bcfc0007133026122549913
Petliuk <130590969+Petliuk@users.noreply.github.com>
2023-05-24 16:03:52 +0300

    make user email unique (#2)
    * make user email unique

--- src/main/java/com/example/myprojectwithspring2/ClientUser/exception/ExceptionController.java
+++ src/main/java/com/example/myprojectwithspring2/ClientUser/exception/ExceptionController.java
@@ -3,34 +3,38 @@ package com.example.myprojectwithspring2.ClientUser.exception;
 
 import com.example.myprojectwithspring2.ClientUser.entity_class.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
-
 import javax.persistence.EntityNotFoundException;
-
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        log.error("handleDataIntegrityViolationException");
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
-        log.info("handleBadRequestException");
+        log.error("handleBadRequestException");
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
-        log.info("handleEntityNotFoundException");
+        log.error("handleEntityNotFoundException");
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
-        log.info("handleException");
+        log.error("handleException");
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
