3f086ecf7ab17f9d5b781e2239c1862959e8d0d9
Maks <petliukofficial@gmail.com>
2023-05-31 11:44:05 +0300

    Added lombok to the Product, ProductCategory and ProductInventory classes and added an empty row in the ProductService class.

--- src/main/java/com/device/shop/entity/Product.java
+++ src/main/java/com/device/shop/entity/Product.java
@@ -1,10 +1,19 @@
 package com.device.shop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class Product {
     @Id
     Long id;
@@ -23,99 +32,5 @@ public class Product {
     @ManyToOne
     ProductCategory productCategory;
 
-    public Product() {
-    }
 
-    public Product(Long id, String name, String description, String SKU, Double price, LocalDateTime created_at, LocalDateTime modified_at, LocalDateTime deleted_at, ProductInventory productInventory, ProductCategory productCategory) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.SKU = SKU;
-        this.price = price;
-        this.created_at = created_at;
-        this.modified_at = modified_at;
-        this.deleted_at = deleted_at;
-        this.productInventory = productInventory;
-        this.productCategory = productCategory;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String desc) {
-        this.description = desc;
-    }
-
-    public String getSKU() {
-        return SKU;
-    }
-
-    public void setSKU(String SKU) {
-        this.SKU = SKU;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public LocalDateTime getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(LocalDateTime created_at) {
-        this.created_at = created_at;
-    }
-
-    public LocalDateTime getModified_at() {
-        return modified_at;
-    }
-
-    public void setModified_at(LocalDateTime modified_at) {
-        this.modified_at = modified_at;
-    }
-
-    public LocalDateTime getDeleted_at() {
-        return deleted_at;
-    }
-
-    public void setDeleted_at(LocalDateTime deleted_at) {
-        this.deleted_at = deleted_at;
-    }
-
-    public ProductInventory getProductInventory() {
-        return productInventory;
-    }
-
-    public void setProductInventory(ProductInventory productInventory) {
-        this.productInventory = productInventory;
-    }
-
-    public ProductCategory getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(ProductCategory productCategory) {
-        this.productCategory = productCategory;
-    }
 }
--- src/main/java/com/device/shop/entity/ProductCategory.java
+++ src/main/java/com/device/shop/entity/ProductCategory.java
@@ -1,6 +1,9 @@
 package com.device.shop.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,6 +14,10 @@ import java.util.List;
 
 @Entity
 @Table(name = "product_category")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class ProductCategory {
     @Id
     Long id;
@@ -23,72 +30,5 @@ public class ProductCategory {
     @OneToMany(mappedBy = "productCategory")
     List<Product> products;
 
-    public ProductCategory() {
-    }
 
-    public ProductCategory(Long id, String name, String description, LocalDateTime created_at, LocalDateTime modified_at, LocalDateTime deleted_at, List<Product> products) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.created_at = created_at;
-        this.modified_at = modified_at;
-        this.deleted_at = deleted_at;
-        this.products = products;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String desc) {
-        this.description = desc;
-    }
-
-    public LocalDateTime getCreated_at() {
-        return created_at;
-    }
-
-    public void setCreated_at(LocalDateTime created_at) {
-        this.created_at = created_at;
-    }
-
-    public LocalDateTime getModified_at() {
-        return modified_at;
-    }
-
-    public void setModified_at(LocalDateTime modified_at) {
-        this.modified_at = modified_at;
-    }
-
-    public LocalDateTime getDeleted_at() {
-        return deleted_at;
-    }
-
-    public void setDeleted_at(LocalDateTime deleted_at) {
-        this.deleted_at = deleted_at;
-    }
-
-    public List<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
 }
